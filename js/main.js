e8313749369f0b14ed4ec9894423e77bd7304a7f
yeahzzl <yeahzzl1018@gmail.com>
2025-01-22 16:34:14 +0900

    feat: 프로모션 작업

--- js/main.js
+++ js/main.js
@@ -47,3 +47,42 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity: 1,
   });
 });
+
+// new Swiper(선택자, 옵션)
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true,
+});
+
+new Swiper(".promotion .swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true, // 1번 슬라이드가 가운데에서 시작
+  loop: true,
+  // autoplay:{
+  //   delay:5000
+  // },
+  pagination: {
+    el: ".promotion .swiper-pagination", // 페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener("click", function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    // 숨김처리
+    promotionEl.classList.add("hide");
+  } else {
+    // 보임처리
+    promotionEl.classList.remove("hide");
+  }
+});
